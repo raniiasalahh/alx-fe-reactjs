04d3ec72e39bf157ccb8ff730c190cf399439dd9
Raniia Salah <113700836+raniiasalahh@users.noreply.github.com>
2024-08-24 04:55:35 +0300

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
-
-
+import RecipeDetails from './components/RecipeDetails';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <div style={{ padding: '20px' }}>
+        <h1>Recipe Sharing App</h1>
+        <SearchBar />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <AddRecipeForm />
+              <RecipeList />
+              <FavoritesList />
+              <RecommendationsList />
+            </>
+          } />
+          <Route path="/recipe/:recipeId" element={<RecipeDetailsWrapper />} />
+        </Routes>
+      </div>
+    </Router>
   );
+}
+
+function RecipeDetailsWrapper() {
+  const { recipeId } = useParams();
+  return <RecipeDetails recipeId={parseInt(recipeId, 10)} />;
 }
 
 export default App;
