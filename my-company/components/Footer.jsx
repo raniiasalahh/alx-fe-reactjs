5489de49dda7fda2dbbad535610f250f0bb8dcb1
Raniia Salah <113700836+raniiasalahh@users.noreply.github.com>
2024-08-24 03:52:45 +0300

    Create Footer.jsx

---
+++ my-company/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ padding: '10px', textAlign: 'center', backgroundColor: '#f8f8f8', marginTop: '20px' }}>
+      <p>&copy; 2024 Our Company</p>
+    </footer>
+  );
+}
+
+export default Footer;
