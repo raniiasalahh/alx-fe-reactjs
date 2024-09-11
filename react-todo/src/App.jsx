100b05dd02cb8ac5c02837443217eb2ca3b49bcd
Raniia Salah <113700836+raniiasalahh@users.noreply.github.com>
2024-09-11 04:25:52 +0300

    Create App.jsx

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+import TodoList from './components/TodoList'; // Import TodoList
+
+function App() {
+  return (
+    <div>
+      <TodoList /> {/* Render TodoList component */}
+    </div>
+  );
+}
+
+export default App;
