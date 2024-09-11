0362434e57adba373ab47c3334f834495dfe319c
Raniia Salah <113700836+raniiasalahh@users.noreply.github.com>
2024-09-11 06:48:38 +0300

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import RecipeDetail from './components/RecipeDetail';
