91da729e08b5b9e31e28432bee88e18d2b45367f
Raniia Salah <113700836+raniiasalahh@users.noreply.github.com>
2024-09-11 06:29:33 +0300

    Create App.jsx

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-blue-500">
+      <h1 className="text-4xl font-bold">Hello, Recipe Sharing Platform!</h1>
+    </div>
+  );
+}
+
+export default App;
