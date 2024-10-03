d94b48891e5e2e9997ec84a8c5f2ad16d0b7b4eb
Raniia Salah <113700836+raniiasalahh@users.noreply.github.com>
2024-10-04 01:00:27 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,13 +1,28 @@
 import axios from 'axios';
 
-const GITHUB_API_URL = 'https://api.github.com/users';
+const GITHUB_API_URL = 'https://api.github.com';
 
 export const fetchUserData = async (username) => {
   try {
-    const response = await axios.get(`${GITHUB_API_URL}/${username}`);
+    const response = await axios.get(`${GITHUB_API_URL}/users/${username}`);
     return response.data;
   } catch (error) {
     console.error('Error fetching user data', error);
+    throw error;
+  }
+};
+
+export const fetchAdvancedUserData = async (username, location, minRepos) => {
+  try {
+    let query = '';
+    if (username) query += `user:${username}`;
+    if (location) query += ` location:${location}`;
+    if (minRepos) query += ` repos:>${minRepos}`;
+
+    const response = await axios.get(`${GITHUB_API_URL}/search/users?q=${query}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching advanced user data', error);
     throw error;
   }
 };
