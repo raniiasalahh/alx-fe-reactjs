a575efea28081910f392d4fd9dfa833a96b2af8f
Raniia Salah <113700836+raniiasalahh@users.noreply.github.com>
2024-10-04 00:33:15 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,31 +1,13 @@
 import axios from 'axios';
 
-// Function to fetch user data by username
+const GITHUB_API_URL = 'https://api.github.com/users';
+
 export const fetchUserData = async (username) => {
-  const response = await axios.get(`https://api.github.com/users/${username}`);
-  return response.data;
-};
-
-// Function to search for users with advanced criteria
-export const searchUsers = async (queryParams) => {
-  const { username, location, minRepos } = queryParams;
-
-  // Construct the base query string
-  let query = `q=${username ? username : ''}`;
-
-  // Append location if provided
-  if (location) {
-    query += `+location:${location}`;
+  try {
+    const response = await axios.get(`${GITHUB_API_URL}/${username}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching user data', error);
+    throw error;
   }
-
-  // Append minimum repositories filter if provided
-  if (minRepos) {
-    query += `+repos:>${minRepos}`;
-  }
-
-  // API endpoint for searching users with the constructed query
-  const response = await axios.get(`https://api.github.com/search/users?${query}`);
-  
-  // Return the array of user objects
-  return response.data.items; 
 };
