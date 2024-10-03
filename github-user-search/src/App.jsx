b5b1ea9c8218a2712584674035f2dcc90bc2904a
Raniia Salah <113700836+raniiasalahh@users.noreply.github.com>
2024-10-04 01:22:42 +0300

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Search from './components/Search';
-import './index.css'; // Include Tailwind CSS
-
-function Home() {
-  return (
-    <div>
-      <h2 className="text-center text-2xl font-bold my-4">GitHub User Search</h2>
-      <Search />
-    </div>
-  );
-}
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/" component={Home} />
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <h1>GitHub User Search</h1>
+      <Search />
+    </div>
   );
 }
 
